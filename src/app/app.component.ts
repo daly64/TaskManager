780499753b53ee1c137292616f1db498c4378d72
Daly <41022166+daly64@users.noreply.github.com>
2024-10-21 21:04:22 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {fakeTasksDatabase} from './fake-tasks-database';
+import {Task} from './interfaces/Task';
+import {DatePipe, NgClass, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgForOf, DatePipe, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'TaskManager';
+  tasks: Task[] = fakeTasksDatabase;
 }
---
+++ src/app/fake-tasks-database.ts
@@ -0,0 +1,18 @@
+import {TaskStatus} from './enums/TaskStatus';
+import { Task } from './interfaces/Task';
+
+// This is a fake database of tasks. It is just an array of tasks that are created here.
+export const fakeTasksDatabase: Task[] = Array.from({ length: 20 }, (_, i) => ({
+  id: i + 1,
+  title: `Task ${i + 1}`,
+  description: `This is the description of task #${i + 1}`,
+  completed: i % 2 === 0,
+  status: (() => {
+    switch (i % 3) {
+      case 0: return TaskStatus.Completed;
+      case 1: return TaskStatus.InProgress;
+      default: return TaskStatus.NotStarted;
+    }
+  })(),
+  dueDate: new Date(Date.now() + (i + 1) * 1000 * 60 * 60 * 24),
+}));
